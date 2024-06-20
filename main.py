175f4e27939bd50b47c581ad8d2188dbf6e0bac7
TomasKopunec <tomas.kopunec5@gmail.com>
2024-06-20 19:12:24 +0100

    Added sample project

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from code2flow.engine import code2flow
 
-project_path = './projects/simple'
-
 code2flow(
-    project_path,
+    raw_source_paths='./projects/users',
     output_dir='output',
     generate_json=True,
     generate_image=True
