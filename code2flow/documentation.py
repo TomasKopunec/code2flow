43b7b04bba63e6cdc72839b13ea68d9b38e04706
TomasKopunec <tomas.kopunec5@gmail.com>
2024-06-22 20:49:15 +0100

    Updated documentation

--- code2flow/documentation.py
+++ code2flow/documentation.py
@@ -3,10 +3,9 @@ class Documentation():
     def get_empty():
         return Documentation()
     
-    def __init__(self, version=0, generated_on='', source_code='', generated_docs=''):
+    def __init__(self, version=0, generated_on='', generated_docs=''):
         self.version = version
         self.generated_on = generated_on
-        self.source_code = source_code
         self.generated_docs = generated_docs
     
     def __str__(self) -> str:
@@ -16,6 +15,5 @@ class Documentation():
         return {
             'version': self.version,
             'generated_on': self.generated_on,
-            'source_code': self.source_code,
             'generated_docs': self.generated_docs
         }
