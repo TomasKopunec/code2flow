3bfd0f9bc258c45764289877863c5a20a7f896dc
Dias2406 <ucabdj5@ucl.ac.uk>
2024-07-11 23:21:01 +0100

    Fix for parent changes

--- code2flow/ast_utils.py
+++ code2flow/ast_utils.py
@@ -35,16 +35,22 @@ class FunctionChange:
                 return f'Function {self.name} has been renamed with similarity of {percent}%'
             case _:
                 raise ValueError(f'Invalid FunctionChangeType: {self.type}')
-
-    def __repr__(self):
-        return f'{self.name}: ({self.type.name}={self.similarity})'
-
-def filter_changes(changes: list[FunctionChange]):
+            
+def filter_changes(changes: list):
     """
-    Filter changes, so that we only retrieve relevant parent dependencies.
+    Filter changes to only retrieve relevant parent dependencies.
     Only include the UPDATED functions.
     """
-    return [change.name for change in changes if change.type == FunctionChangeType.UPDATED]
+    filtered_changes = []
+    print(f"Changes: {changes}")
+    for change in changes:
+        # Check if each 'change' is a dictionary and has the required keys
+        print(f"Change: {change.type}")
+        if change.type == FunctionChangeType.UPDATED:
+            filtered_changes.append(change.name)
+        else:
+            print(f"Invalid change format: {change}")
+    return filtered_changes
 
 
 def get_function_changes(file_path, old_file, new_file) -> list[FunctionChange]:
@@ -96,6 +102,9 @@ def get_function_changes(file_path, old_file, new_file) -> list[FunctionChange]:
             similarity.append(FunctionChange(
                 old_name, FunctionChangeType.REMOVED, 0))
 
+    for simi in similarity:
+        print(f"Function: {simi.name}")
+        print(f"Type: {simi.type}")
     return similarity
 
 
